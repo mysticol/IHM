8ed4cdfbe11d25b9dbc01d7aaf70dad2e0347ac6
JulienSambre <JulienSambre@e9c24a4e-4ec7-6425-f316-564117deb09f>
2010-11-08 18:38:00 +0000

    new logwriter

--- risque_peril/composant/HADL/src/hadl/lib/LogWriter.java
+++ risque_peril/composant/HADL/src/hadl/lib/LogWriter.java
@@ -12,6 +12,9 @@ public class LogWriter {
 	
 	private static HashMap<String, PrintStream> logMap = new HashMap<String, PrintStream>();
 	private static LogWriter instance = null;
+	private static String idUnique = null;
+	private static PrintStream psUnique = null;
+	private static boolean logUnique = false;
 	
 	/* windows path
 	private String path = "src\\log\\"; */
@@ -45,6 +48,24 @@ public class LogWriter {
 	}
 	public void setPath(String path) {
 		this.path = path;
+	}
+	public static void setIdUnique(String idUnique) {
+		LogWriter.idUnique = idUnique;
+	}
+	public static String getIdUnique() {
+		return idUnique;
+	}
+	public static void setPsUnique(PrintStream psUnique) {
+		LogWriter.psUnique = psUnique;
+	}
+	public static PrintStream getPsUnique() {
+		return psUnique;
+	}
+	public static void setLogUnique(boolean logUnique) {
+		LogWriter.logUnique = logUnique;
+	}
+	public static boolean isLogUnique() {
+		return logUnique;
 	}
 
 	public static LogWriter getInstance() {
@@ -88,6 +109,41 @@ public class LogWriter {
 		
 	}
 	
+	public void initUnique(String id){
+		// On associe un id a un fichier de log
+		
+        FileOutputStream out; // declare a file output object
+        PrintStream p; // declare a print stream object
+
+        try
+        {
+        	// On test si le directory path existe
+        	// Si non, on le créé
+        	File dir=new File(path);
+        	if(!dir.exists()){
+        		dir.mkdir();
+        	}
+        	
+            // Create a new file output stream
+            // connected to "myfile.txt"
+            out = new FileOutputStream(path + id + ".log");
+
+            // Connect print stream to the output stream
+            p = new PrintStream( out );
+           
+            idUnique = id;
+            psUnique = p;
+            
+            logUnique = true;
+        }
+        catch (Exception e)
+        {
+                System.err.println ("Error writing to file");
+        }				
+		
+		
+	}
+	
 	public void open(String id){
 		// On associe un id a un fichier de log
 		
@@ -112,10 +168,36 @@ public class LogWriter {
 		
 	}
 	
+	public void open(){
+		// On associe un id a un fichier de log
+		
+        FileOutputStream out; // declare a file output object
+        PrintStream p; // declare a print stream object
+
+        try
+        {
+            // Create a new file output stream
+            // connected to "myfile.txt"
+            out = new FileOutputStream(new File(path + idUnique + ".log"), true);
+
+            // Connect print stream to the output stream
+            p = new PrintStream( out );
+        }
+        catch (Exception e)
+        {
+                System.err.println ("Error writing to file");
+        }			
+	}	
+	
 	public void close(String id){
 		// On ferme le fichier de log associe à l'id
 		logMap.get(id).close();
 	}
+	
+	public void close(){
+		// On ferme le fichier de log associe à l'id
+		psUnique.close();
+	}	
 	
 	public void write(String id, String log){
 		// On ecrit dans le log file
@@ -126,6 +208,31 @@ public class LogWriter {
 		}
 	}
 	
+	public void write(String log){
+		// On écrit dans le log file
+		if (withDate) {
+			psUnique.println(dateFormat.format(new Date()) + "  |  " +log);
+		} else {
+			psUnique.println(log);
+		}
+	}
 	
+	public void writejl(String id, String log){
+		write(id, log);
+		logMap.get(id).println("");
+	}
+	
+	public void writejl(String log){
+		write(log);
+		psUnique.println("");
+	}
+	
+	public void writesep(String id){
+		logMap.get(id).println("---------------------------------------------------------------------");
+	}
+	
+	public void writesep(){
+		psUnique.println("---------------------------------------------------------------------");
+	}	
 
 }
