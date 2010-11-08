e03dff205ad36772ca5a79884612ef91ea15ca64
JulienSambre <JulienSambre@e9c24a4e-4ec7-6425-f316-564117deb09f>
2010-11-08 16:09:18 +0000

    parser xml + dtd

---
+++ risque_peril/composant/HADL/src/hadl/m0/descriptionLangage/GestionXml.java
@@ -0,0 +1,56 @@
+package hadl.m0.descriptionLangage;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+
+public class GestionXml {
+	
+		org.jdom.Document document;
+		static Element racine;
+	
+	public GestionXml() {
+		
+	      //On crée une instance de SAXBuilder
+		  SAXBuilder sxb = new SAXBuilder(true);
+		  try
+		      {
+	         //On crée un nouveau document JDOM avec en argument le fichier XML
+	         //Le parsing est terminé ;)			 
+	         document = sxb.build(new File("src/parser/xml/exempleHadl.xml"));
+	      }
+	      catch(Exception e){}
+
+	      //On initialise un nouvel élément racine avec l'élément racine du document.
+	      racine = document.getRootElement();
+
+	      //Méthode définie dans la partie 3.2. de cet article
+	      afficheALL();
+
+	}
+	
+	//Ajouter cette méthodes à la classe JDOM2
+	static void afficheALL()
+	{
+	   //On crée une List contenant tous les noeuds "etudiant" de l'Element racine
+	   Element composants = racine.getChild("composants");
+
+	   List listComposants = composants.getChildren();
+	   
+	   //On crée un Iterator sur notre liste
+	   Iterator i = listComposants.iterator();
+	   while(i.hasNext())
+	   {
+	      //On recrée l'Element courant à chaque tour de boucle afin de
+	      //pouvoir utiliser les méthodes propres aux Element comme :
+	      //selectionner un noeud fils, modifier du texte, etc...
+	      Element courant = (Element)i.next();
+	      //On affiche le nom de l'element courant
+	      System.out.println(courant.getAttributeValue("nom"));
+	   }
+	}
+
+}
---
+++ risque_peril/composant/HADL/src/hadl/m0/descriptionLangage/MainParser.java
@@ -0,0 +1,13 @@
+package hadl.m0.descriptionLangage;
+
+public class MainParser {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		GestionXml test = new GestionXml();
+
+	}
+
+}
