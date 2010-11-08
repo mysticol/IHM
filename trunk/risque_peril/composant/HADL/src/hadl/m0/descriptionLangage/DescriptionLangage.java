a846b8fad6525b6b636cc4c8201f339f88ab927f
JulienSambre <JulienSambre@e9c24a4e-4ec7-6425-f316-564117deb09f>
2010-11-08 18:38:00 +0000

    new logwriter

--- trunk/risque_peril/composant/HADL/src/hadl/m0/descriptionLangage/DescriptionLangage.java
+++ trunk/risque_peril/composant/HADL/src/hadl/m0/descriptionLangage/DescriptionLangage.java
@@ -1,5 +1,7 @@
 package hadl.m0.descriptionLangage;
 
+import hadl.lib.LogWriter;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -10,14 +12,20 @@ import org.jdom.input.SAXBuilder;
 public class DescriptionLangage {
 	
 	private Document document;
+	private LogWriter lw = LogWriter.getInstance();
 	
 	public DescriptionLangage(String filePath){
 		
+		//On créé le fichier de log
+		lw.initUnique("descriptionLangage");
+		
 		try {
-			
 			// On stocke dans la classe
 			SAXBuilder sxb = new SAXBuilder(true);
 			this.document = sxb.build(new File(filePath));
+			
+			lw.write("Stockage du xml en mémoire");
+			lw.writesep();
 			
 		} catch (JDOMException e) {
 			e.printStackTrace();
