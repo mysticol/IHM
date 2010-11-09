6cd492549cf2136261d4ed136dac1d5166440fbf
JulienSambre <JulienSambre@e9c24a4e-4ec7-6425-f316-564117deb09f>
2010-11-09 18:44:22 +0000

    Version stable du parser. On arrive à générer toutes nos classes sans problème. On arrive à les utiliser sans problème aussi.

--- trunk/risque_peril/composant/HADL/src/hadl/m0/descriptionLangage/MainDescriptionLangage.java
+++ trunk/risque_peril/composant/HADL/src/hadl/m0/descriptionLangage/MainDescriptionLangage.java
@@ -1,13 +1,17 @@
 package hadl.m0.descriptionLangage;
 
+import hadl.m1.CS.CS;
+import hadl.m2.configuration.Configuration;
+
 public class MainDescriptionLangage {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		DescriptionLangage test = new DescriptionLangage("src/hadl/m0/descriptionLangage/exempleHadl.xml");
-
+		DescriptionLangage test = new DescriptionLangage("src/hadl/m0/descriptionLangage/exempleCS.xml");
+		CS testConf = (CS) test.analyse();
+		testConf.start();
 	}
 
 }
