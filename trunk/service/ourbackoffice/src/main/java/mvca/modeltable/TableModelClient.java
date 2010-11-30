3a6d23da9668afeb89acf9657b0c69155c8ea4e4
manoel.fortun@gmail.com <manoel.fortun@gmail.com@e9c24a4e-4ec7-6425-f316-564117deb09f>
2010-11-30 13:02:56 +0000

    Back office le retour de la mort qui tue.

---
+++ trunk/service/ourbackoffice/src/main/java/mvca/modeltable/TableModelClient.java
@@ -0,0 +1,86 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvca.modeltable;
+
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import mvca.entity.Client;
+
+
+
+/**
+ *
+ * @author Mister B
+ */
+public class TableModelClient extends AbstractTableModel {
+
+    private List<Client> listClient;
+   // private ClientJpaController entity;
+
+    public TableModelClient() {
+        //this.entity = new ClientJpaController();
+        this.refresh();
+    }
+
+    public void refresh() {
+      //  listClient = entity.findClientEntities();
+
+    }
+
+    @Override
+    public int getRowCount() {
+        return listClient.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+
+        switch (columnIndex) {
+
+            case 0:
+                return listClient.get(rowIndex).getClientId();
+
+            case 1:
+                return listClient.get(rowIndex).getNomClient();
+
+
+
+            case 2:
+                return listClient.get(rowIndex).getPrenomClient();
+            default:
+                return (String) "Rien";
+
+        }
+    }
+
+    @Override
+    public String getColumnName(int column) {
+
+         switch (column) {
+            case 0:
+                return "ID";
+
+            case 1:
+                return "nom";
+
+
+
+            case 2:
+                return "Prenom";
+            default:
+                return (String) "Rien";
+
+        }
+
+    }
+
+
+
+}
