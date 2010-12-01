146e159cc7d1fd728e29d3ab5dd72cfb6b722a78
manoel.fortun@gmail.com <manoel.fortun@gmail.com@e9c24a4e-4ec7-6425-f316-564117deb09f>
2010-12-01 14:46:39 +0000

    Avancé dans l'ihm

--- service/ourbackoffice/src/main/java/mvca/vue/mainFrame.java
+++ service/ourbackoffice/src/main/java/mvca/vue/mainFrame.java
@@ -15,10 +15,14 @@ import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.util.HashMap;
 import javax.swing.JButton;
 import javax.swing.JPanel;
-import mvca.controleur.controleurClient;
+import mvca.controleur.ControleurClient;
+import mvca.controleur.ControleurRadio;
 import mvca.entity.Client;
+import mvca.model.Mode;
+import mvca.model.ModeParam;
 import mvca.modeltable.TableModelClient;
 import mvca.session.HibernateUtil;
 import org.hibernate.Hibernate;
@@ -28,6 +32,10 @@ import org.hibernate.Hibernate;
  * @author Mister B
  */
 public class mainFrame extends javax.swing.JFrame {
+
+
+    private ControleurRadio radioControleur;
+    private HashMap<Mode, ModeParam> modeur;
 
     /** Creates new form mainFrame */
     public mainFrame() {
@@ -206,8 +214,7 @@ public class mainFrame extends javax.swing.JFrame {
             public void run() {
                 mainFrame mv=  new mainFrame();
 
-            mv.listenerEttout();
-
+            mv.init();
                 mv.setVisible(true);
                 
 
@@ -239,7 +246,7 @@ public class mainFrame extends javax.swing.JFrame {
  
 public void placeMachin(JPanel pane){
     this.panelAmovible.removeAll();
-
+this.panelAmovible.repaint();
     GridBagLayout fl= new GridBagLayout();
    
     panelAmovible.setLayout(fl);
@@ -247,52 +254,108 @@ public void placeMachin(JPanel pane){
        GridBagConstraints constraint= new GridBagConstraints();
        constraint.fill= GridBagConstraints.BOTH;
         fl.setConstraints(pane, constraint);
-    panelAmovible.repaint();
+       
+   
+   panelAmovible.revalidate();
  
 
 }
 
 
-public void listenerEttout(){
 
-    jClientPane jj=new jClientPane();
+
+
+
+
+public void setMode(Mode m){
+
+ModeParam mp=this.modeur.get(m);
+this.placeMachin(mp.getPanel());
+this.superJtableBD.setModel(mp.getModel());
+this.superJtableBD.removeAll();
+this.superJtableBD.addMouseListener(mp.getAction());
+
+
+
+}
+
+    private void init() {
+
+         jClientPane clientPane=new jClientPane();
+         jHotelPane hotelPane= new jHotelPane();
+         jRestaurantPane restaurantPane= new jRestaurantPane();
+         jVoyagePane voyagePane=new jVoyagePane();
+         jManifestationPane manifPane= new jManifestationPane();
+         jTypeManifPane typeManifPane= new jTypeManifPane();
+         jLocalisationPane localisationPane= new jLocalisationPane();
+         jReservationHotelPane reservHotelPane= new jReservationHotelPane();
+         jReservationRestaurantPane reservRestauPane= new jReservationRestaurantPane();
+         jReservationManifPane reservationManifPane= new jReservationManifPane();
+         jReservationnPane reservationnPane= new jReservationnPane();
+
+
+
     HibernateUtil<Client> em= new HibernateUtil<Client>(Client.class);
     TableModelClient gg= new TableModelClient(em);
 
-    controleurClient control= new controleurClient();
-    
-    
+    ControleurClient control= new ControleurClient();
+
+
 
     control.setEm(em);
     control.setModel(gg);
     control.setTable(this.superJtableBD);
-    control.setClientControleur(jj);
+    control.setClientControleur(clientPane);
 
 
-    superJtableBD.setModel(gg);
-    superJtableBD.addMouseListener(control.getTableModelListener());
 
-    /*
-     *
-     *   public JButton getEditButton() {
-        return editButton;
+
+    clientPane.getNewButton().addActionListener(control.getNewActionListener());
+    clientPane.getSaveButton().addActionListener(control.getSaveActionListener());
+
+
+
+
+
+
+
+
+        this.radioControleur= new ControleurRadio(this);
+        this.modeur=new HashMap<Mode, ModeParam>();
+
+
+
+
+        modeur.put(Mode.Client, new ModeParam(control.getTableModelListener(),gg,clientPane));
+        modeur.put(Mode.Hotel, new ModeParam(null, gg, hotelPane));
+        modeur.put(Mode.Restaurant, new ModeParam(null, gg, restaurantPane));
+        modeur.put(Mode.Localisation, new ModeParam(null, gg, localisationPane));
+        modeur.put(Mode.TypeManif, new ModeParam(null, gg, typeManifPane));
+        modeur.put(Mode.Voyage, new ModeParam(null, gg, voyagePane));
+        modeur.put(Mode.Manifestation, new ModeParam(null, gg, manifPane));
+        modeur.put(Mode.Reservation, new ModeParam(null, gg, reservationnPane));
+        modeur.put(Mode.ReservationHotel, new ModeParam(null, gg, reservHotelPane));
+        modeur.put(Mode.ReservationRestaurant, new ModeParam(null, gg, reservRestauPane));
+        modeur.put(Mode.ReservationManif, new ModeParam(null, gg, reservationManifPane));
+
+
+        this.clientRadioButton.addActionListener(radioControleur.getClientListener());
+        this.hotelRadioButton.addActionListener(radioControleur.getHotelListener());
+        this.restaurantRadioButton.addActionListener(radioControleur.getRestaurantListener());
+        this.reservHotelRadioButton.addActionListener(radioControleur.getReservationHotelListener());
+        this.reservManifRadioButton.addActionListener(radioControleur.getReservationManifestationListener());
+        this.reservationRadioButton.addActionListener(radioControleur.getReservationListener());
+        this.reservRestauRadioButton.addActionListener(radioControleur.getReservationRestaurantListener());
+        this.localisationRadioButton.addActionListener(radioControleur.getLocalisationListener());
+        this.voyageRadioButton.addActionListener(radioControleur.getLocalisationListener());
+        this.typeManifRadioButton.addActionListener(radioControleur.getTypeManifestationListener());
+        this.manifestationRadioButton.addActionListener(radioControleur.getManifestationListener());
+
+
+        
+
+          
+        this.setMode(Mode.Hotel);
     }
-
-    public JButton getNewButton() {
-        return newButton;
-    }
-
-    public JButton getSaveButton() {
-        return saveButton;
-     *
-     */
-
-    
-    jj.getNewButton().addActionListener(control.getNewActionListener());
-    jj.getSaveButton().addActionListener(control.getSaveActionListener());
-
-this.placeMachin(jj);
-    
-}
 
 }
