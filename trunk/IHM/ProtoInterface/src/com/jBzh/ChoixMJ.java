36b4469d32db95eed5855b640e6a4e7297b0fba8
anthony.caillaud <anthony.caillaud@e9c24a4e-4ec7-6425-f316-564117deb09f>
2010-12-08 10:18:28 +0000

    Ouverture d'une nouvelle fenêtre

---
+++ trunk/IHM/ProtoInterface/src/com/jBzh/ChoixMJ.java
@@ -0,0 +1,19 @@
+package com.jBzh;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChoixMJ extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+    	//on lui associe le layout afficahgequestionactivity.xml
+    	setContentView(R.layout.choixmj);
+
+	}
+	
+
+}
--- trunk/IHM/ProtoInterface/src/com/jBzh/ProtoInterface.java
+++ trunk/IHM/ProtoInterface/src/com/jBzh/ProtoInterface.java
@@ -1,18 +1,39 @@
 package com.jBzh;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class ProtoInterface extends Activity {
+	
+	private static final int CODE_DE_MON_ACTIVITE = 1;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
-    
-    public void choixMJ(View view) {
-        System.out.println("Choix MJ!Wouhouuuuuuuu!!!!");
+        
+        final Button button = (Button) findViewById(R.id.MJ);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	
+            	//On créé un objet Bundle, c'est ce qui va nous permetre d'envoyer des données à l'autre Activity
+    			Bundle objetbunble = new Bundle();
+     
+    			//On créé l'Intent qui va nous permettre d'afficher l'autre Activity
+    			Intent intent = new Intent(ProtoInterface.this, ChoixMJ.class);
+     
+    			//On affecte à l'Intent le Bundle que l'on a créé
+    			intent.putExtras(objetbunble);
+     
+    			//On démarre l'autre Activity
+    			startActivityForResult(intent, CODE_DE_MON_ACTIVITE);
+
+            }
+        });
+
     }
 }
