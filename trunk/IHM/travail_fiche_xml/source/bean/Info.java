4cbde99e1bc5c395af49fd034b4ea7688c27e0f2
manoel.fortun@gmail.com <manoel.fortun@gmail.com@e9c24a4e-4ec7-6425-f316-564117deb09f>
2010-12-21 17:33:15 +0000

    Parseur de modèle fini, il renvoi une fiche vierge avec tout les éléments a remplir.

---
+++ trunk/IHM/travail_fiche_xml/source/bean/Info.java
@@ -0,0 +1,30 @@
+package bean;
+
+public class Info {
+
+	private String nom;
+	private String value;
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public Info(String nom, String value) {
+		this.nom = nom;
+		this.value = value;
+	}
+	public Info() {
+		
+	}
+	
+	
+	
+	
+}
--- trunk/IHM/travail_fiche_xml/source/parseur/ParseurFiche.java
+++ trunk/IHM/travail_fiche_xml/source/parseur/ParseurFiche.java
@@ -1,5 +1,24 @@
 package parseur;
 
+import org.jdom.Element;
+
+import bean.Equipement;
+import bean.Pouvoir;
+
 public class ParseurFiche {
 
+	public Equipement parseEquipement(Element e){
+		Equipement result= new Equipement();
+		
+		return result;
+	}
+	
+	
+	public Pouvoir parsePouvoir(Element e ){
+		Pouvoir result= new Pouvoir();
+		
+		return result;
+	}
+	
+	
 }
