855fb1cb8b8559f5f37d44e39470f4cdbc2cbbf5
anthony.caillaud <anthony.caillaud@e9c24a4e-4ec7-6425-f316-564117deb09f>
2010-12-22 14:18:59 +0000

    Rajout de listes sur ProtoInterface

--- trunk/IHM/ProtoInterface/src/com/jBzh/ChoixMJ.java
+++ trunk/IHM/ProtoInterface/src/com/jBzh/ChoixMJ.java
@@ -2,6 +2,8 @@ package com.jBzh;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ChoixMJ extends Activity{
 
@@ -12,6 +14,18 @@ public class ChoixMJ extends Activity{
 		
     	//on lui associe le layout afficahgequestionactivity.xml
     	setContentView(R.layout.choixmj);
+    	
+    	Spinner UniversSpinner = (Spinner) findViewById(R.id.UniversSpinner);
+        ArrayAdapter<CharSequence> adapterU = ArrayAdapter.createFromResource(
+                this, R.array.univers_array, android.R.layout.simple_spinner_item);
+        adapterU.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        UniversSpinner.setAdapter(adapterU);
+        
+        Spinner campagneSpinner = (Spinner) findViewById(R.id.CampagneSpinner);
+        ArrayAdapter<CharSequence> adapterC = ArrayAdapter.createFromResource(
+                this, R.array.univers_array, android.R.layout.simple_spinner_item);
+        adapterC.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        campagneSpinner.setAdapter(adapterC);
 
 	}
 	
