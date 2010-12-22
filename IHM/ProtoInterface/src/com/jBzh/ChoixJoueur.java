8f468bdd2c9a048b9edfbcd59c759faa2c1507fc
anthony.caillaud <anthony.caillaud@e9c24a4e-4ec7-6425-f316-564117deb09f>
2010-12-22 14:18:59 +0000

    Rajout de listes sur ProtoInterface

---
+++ IHM/ProtoInterface/src/com/jBzh/ChoixJoueur.java
@@ -0,0 +1,33 @@
+package com.jBzh;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class ChoixJoueur extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		//on lui associe le layout afficahgequestionactivity.xml
+    	setContentView(R.layout.choixjoueur);
+    	
+    	Spinner UniversSpinner = (Spinner) findViewById(R.id.UniversSpinner);
+        ArrayAdapter<CharSequence> adapterU = ArrayAdapter.createFromResource(
+                this, R.array.univers_array, android.R.layout.simple_spinner_item);
+        adapterU.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        UniversSpinner.setAdapter(adapterU);
+        
+        Spinner campagneSpinner = (Spinner) findViewById(R.id.CampagneSpinner);
+        ArrayAdapter<CharSequence> adapterC = ArrayAdapter.createFromResource(
+                this, R.array.univers_array, android.R.layout.simple_spinner_item);
+        adapterC.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        campagneSpinner.setAdapter(adapterC);
+	}
+	
+	
+
+}
--- IHM/ProtoInterface/src/com/jBzh/ProtoInterface.java
+++ IHM/ProtoInterface/src/com/jBzh/ProtoInterface.java
@@ -16,8 +16,8 @@ public class ProtoInterface extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        final Button button = (Button) findViewById(R.id.MJ);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Button buttonMJ = (Button) findViewById(R.id.MJ);
+        buttonMJ.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
             	
             	//On créé un objet Bundle, c'est ce qui va nous permetre d'envoyer des données à l'autre Activity
@@ -34,6 +34,24 @@ public class ProtoInterface extends Activity {
 
             }
         });
+        
+        final Button buttonJoueur = (Button) findViewById(R.id.Joueur);
+        buttonJoueur.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	
+            	//On créé un objet Bundle, c'est ce qui va nous permetre d'envoyer des données à l'autre Activity
+    			Bundle objetbunble = new Bundle();
+     
+    			//On créé l'Intent qui va nous permettre d'afficher l'autre Activity
+    			Intent intent = new Intent(ProtoInterface.this, ChoixJoueur.class);
+     
+    			//On affecte à l'Intent le Bundle que l'on a créé
+    			intent.putExtras(objetbunble);
+     
+    			//On démarre l'autre Activity
+    			startActivityForResult(intent, CODE_DE_MON_ACTIVITE);
 
+            }
+        });
     }
 }
