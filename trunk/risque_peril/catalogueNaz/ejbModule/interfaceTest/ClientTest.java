8dab5850f673fb1e1826b57be3412d0670fd35cf
dejean.charles <dejean.charles@e9c24a4e-4ec7-6425-f316-564117deb09f>
2010-12-27 12:28:13 +0000

    maj test

--- trunk/risque_peril/catalogueNaz/ejbModule/interfaceTest/ClientTest.java
+++ trunk/risque_peril/catalogueNaz/ejbModule/interfaceTest/ClientTest.java
@@ -63,7 +63,6 @@ public class ClientTest {
 					break;
 				case 2 : // Récupérer les produits d'une marque
 					 	 System.out.print("--------> marque : ");
-						 marque = myInput.readLine();
 						 for( Produit p : catalogueService.findByMarque(marque)){
 							 printProduit(p);
 						 }
