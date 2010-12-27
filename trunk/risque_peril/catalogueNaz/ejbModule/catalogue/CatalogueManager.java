4ba43029779dfa5b0ca7a8e2b8b1cc45e2af8a4a
dejean.charles <dejean.charles@e9c24a4e-4ec7-6425-f316-564117deb09f>
2010-12-27 14:50:08 +0000

    ajout classe d'Init

--- trunk/risque_peril/catalogueNaz/ejbModule/catalogue/CatalogueManager.java
+++ trunk/risque_peril/catalogueNaz/ejbModule/catalogue/CatalogueManager.java
@@ -29,14 +29,7 @@ public class CatalogueManager {
 		
 		Context context = new InitialContext();
         // Nom de la classe d'implémentation + /local ou /remote
-        CatalogueServiceRemote catalogueLocal = (CatalogueServiceRemote) context.lookup("CatalogueService/remote");
-        
-        // On s'enregistre aupres du server central
-//        CatalogueRegisteringRemote centralServerRegistering = (CatalogueRegisteringRemote) context.lookup("CatalogueRegistering/remote");
-//	
-//        if(!centralServerRegistering.senregistrer("", "dejean-pottier", "CatalogueService")){
-//        	System.out.println("Probleme : impossible de s'enregistrer aupres du server central");
-//        }        
+        CatalogueServiceRemote catalogueLocal = (CatalogueServiceRemote) context.lookup("CatalogueService/remote"); 
         
     	String categorie;
 		String marque;
@@ -45,19 +38,6 @@ public class CatalogueManager {
 		Integer quantite;
 		String description;
 		Long id;
-        
-		// création automatique du catalogue pour les tests !!
-		catalogueLocal.addCategorie("Jouet");
-        catalogueLocal.addCategorie("Jardin"); 
-        
-        catalogueLocal.addProduct(" vrai osselet juif ", 10.0, "Jouet", "Les Restes de la fosses", "osselets", 500);
-        catalogueLocal.addProduct(" vrai toupie juive ", 15.0, "Jouet", "Les Restes de la fosses", "toupie", 100);
-        catalogueLocal.addProduct(" fait avec de vrai plombage ", 12.0, "Jouet", "Les Restes du four", "soldat de plomb", 100);
-        
-        catalogueLocal.addProduct(" un joli rateau en os ", 30.0, "Jardin", "Les Restes de la fosses", "rateau", 100);
-        catalogueLocal.addProduct(" un bon angraie plein de bonne chose ", 20.0, "Jardin", "Les Restes du four", "angraie", 900);
-        
-        // fin de la creation du catalogue
         
         // Menu admin
         do{
