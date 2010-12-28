7bd75a4c95e0e7bdcb1714fb0e9a56c7b66a8d96
anthony.caillaud <anthony.caillaud@e9c24a4e-4ec7-6425-f316-564117deb09f>
2010-12-28 14:57:22 +0000

    Classe pour creation

---
+++ IHM/ProtoInterface/src/com/jBzh/Creation.java
@@ -0,0 +1,69 @@
+package com.jBzh;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class Creation extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		//on lui associe le layout afficahgequestionactivity.xml
+    	setContentView(R.layout.creation);
+    	
+    	Spinner UniversSpinner = (Spinner) findViewById(R.id.UniversSpinner);
+        ArrayAdapter<CharSequence> adapterU = ArrayAdapter.createFromResource(
+                this, R.array.univers_array, android.R.layout.simple_spinner_item);
+        adapterU.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        UniversSpinner.setAdapter(adapterU);
+        
+        
+        final Button buttonRetour = (Button) findViewById(R.id.retour);
+        buttonRetour.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	
+            	//On créé un objet Bundle, c'est ce qui va nous permetre d'envoyer des données à l'autre Activity
+    			Bundle objetbunble = new Bundle();
+     
+    			//On créé l'Intent qui va nous permettre d'afficher l'autre Activity
+    			Intent intent = new Intent(Creation.this, ProtoInterface.class);
+     
+    			//On affecte à l'Intent le Bundle que l'on a créé
+    			intent.putExtras(objetbunble);
+     
+    			//On démarre l'autre Activity
+    			startActivityForResult(intent, 1);
+
+            }
+        });
+        
+        final Button buttonValider = (Button) findViewById(R.id.valider);
+        buttonRetour.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	
+            	//On créé un objet Bundle, c'est ce qui va nous permetre d'envoyer des données à l'autre Activity
+    			Bundle objetbunble = new Bundle();
+     
+    			//On créé l'Intent qui va nous permettre d'afficher l'autre Activity
+    			Intent intent = new Intent(Creation.this, CreationParam.class);
+     
+    			//On affecte à l'Intent le Bundle que l'on a créé
+    			intent.putExtras(objetbunble);
+     
+    			//On démarre l'autre Activity
+    			startActivityForResult(intent, 1);
+
+            }
+        });
+	}
+	
+	
+
+}
