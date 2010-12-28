434c5d8c4b43c64bda06bac00a2e69078bfc603d
dejean.charles <dejean.charles@e9c24a4e-4ec7-6425-f316-564117deb09f>
2010-12-28 16:50:49 +0000

    ajout d'autre type e jet , explosif et explosif avec option

--- trunk/IHM/DiceModul/src/dice/Dice.java
+++ trunk/IHM/DiceModul/src/dice/Dice.java
@@ -4,11 +4,9 @@ import java.util.LinkedList;
 
 public class Dice {
 
-	
-	
 	public static LinkedList<Integer> roll( int nbDice, DiceType dt){
-		LinkedList<Integer> listresult= new LinkedList<Integer>();
 		
+		LinkedList<Integer> listresult= new LinkedList<Integer>();
 		
 		int lower = 1;
 		int higher = dt.getMaxForDiceType();
@@ -17,15 +15,60 @@ public class Dice {
 			
 			listresult.add((int)(Math.random() * (higher+1-lower)) + lower);
 		}
-
-		
-		
-		
-		
 		
 		return listresult;
 	}
 	
+	/**
+	 * Les dés sont relancé temps qu'il font la valeur maximal possible, il explose
+	 * @param nbDice
+	 * @param dt
+	 * @return
+	 */
+	public static LinkedList<Integer> rollExplosive( int nbDice, DiceType dt){
+		
+		LinkedList<Integer> listresult= new LinkedList<Integer>();
+		
+		int lower = 1;
+		int higher = dt.getMaxForDiceType();
+		
+		
+		for (int i=0; i<nbDice; i++){
+			do{
+				listresult.add((int)(Math.random() * (higher+1-lower)) + lower);
+			}while(listresult.getLast() == higher);
+		}
+		
+		return listresult;
+	}
 	
+	/**
+	 * Les dés explosent, mais on peut dire combien d'entre eux explose et donner une valeur minimal d'explosion
+	 * @param nbDice : nombre de dès lancé
+	 * @param dt : type de dés
+	 * @param nbDiceExplosive : nombre de dés explosif ( ne doit pas etre superieur a nbDice )
+	 * @param valMinExplosive : valeur minimal sur la laquel un dés peut explosé (ne doit pas etre superieur au type de dés lancé)
+	 * @return
+	 */
+	public static LinkedList<Integer> rollExplosiveWithOption( int nbDice, DiceType dt, int nbDiceExplosive, int valMinExplosive  ){
+		
+		LinkedList<Integer> listresult= new LinkedList<Integer>();
+		
+		int lower = 1;
+		int higher = dt.getMaxForDiceType();
+		
+		
+		for (int i=0; i<nbDiceExplosive; i++){
+			do{
+				listresult.add((int)(Math.random() * (higher+1-lower)) + lower);
+			}while(listresult.getLast() >= valMinExplosive);
+		}
+		
+		for (int i=nbDiceExplosive; i<nbDice; i++){
+			listresult.add((int)(Math.random() * (higher+1-lower)) + lower);
+		}		
+		
+		return listresult;
+	}
 	
 }
--- trunk/IHM/DiceModul/src/dice/maintest.java
+++ trunk/IHM/DiceModul/src/dice/maintest.java
@@ -2,19 +2,17 @@ package dice;
 
 public class maintest {
 
-	
 	public static void main(String[] args) {
 
 		DiceType dsix= DiceType.D6;
 		
 		System.out.println(dsix.getMaxForDiceType());
-		
-		
+
 		System.out.println(Dice.roll(10, dsix));
 		
+		System.out.println(Dice.rollExplosive(10, dsix));
 		
-		
-		
+		System.out.println(Dice.rollExplosiveWithOption(10, dsix, 2 , 4));
 	}
 	
 }
