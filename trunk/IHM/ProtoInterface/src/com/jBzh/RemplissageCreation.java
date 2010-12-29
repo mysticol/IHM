033063e2063e2431ecee5b3952d645b27a212e0b
anthony.caillaud <anthony.caillaud@e9c24a4e-4ec7-6425-f316-564117deb09f>
2010-12-29 13:29:44 +0000

    Ajout remplissageCreation et modifications dans l'Adapter

--- trunk/IHM/ProtoInterface/src/com/jBzh/RemplissageCreation.java
+++ trunk/IHM/ProtoInterface/src/com/jBzh/RemplissageCreation.java
@@ -1,9 +1,14 @@
 package com.jBzh;
 
+import java.util.ArrayList;
+
+import com.jBzh.CreationNumericAdapter.CreationNumericAdapterListener;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class RemplissageCreation extends Activity{
+public class RemplissageCreation extends Activity implements CreationNumericAdapterListener{
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -11,8 +16,34 @@ public class RemplissageCreation extends Activity{
 	  
 	  setContentView(R.layout.remplissagecreation);
 	  
+	  //liste de Numeric
+	  ArrayList<Numeric> listN = new ArrayList<Numeric>();
+	  
+	  listN.add(new Numeric("Force",0));
+	  listN.add(new Numeric("Agilite",0));
 	  
 	  
+      CreationNumericAdapter adapter = new CreationNumericAdapter(this, listN);
+     
+      adapter.addListener(this);
+      
+      ListView list = (ListView)findViewById(R.id.list);
+      
+      list.setAdapter(adapter);
+	  
+	  
+	}
+
+	@Override
+	public void onClickMoins(Numeric item, int position) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onClickPlus(Numeric item, int position) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	
