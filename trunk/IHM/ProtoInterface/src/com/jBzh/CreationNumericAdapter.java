4a6c0711b942e4bdb53af2ae24154cd0c0c3120e
JulienSambre <JulienSambre@e9c24a4e-4ec7-6425-f316-564117deb09f>
2010-12-29 12:30:13 +0000

    pour bambinome

---
+++ trunk/IHM/ProtoInterface/src/com/jBzh/CreationNumericAdapter.java
@@ -0,0 +1,50 @@
+package com.jBzh;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class CreationNumericAdapter extends BaseAdapter {
+	
+	private List<Numeric> listNumeric;
+	
+	//Le contexte dans lequel est présent notre adapter
+	private Context mContext;
+	    	
+	//Un mécanisme pour gérer l'affichage graphique depuis un layout XML
+	private LayoutInflater mInflater;
+	
+	// Constructeur
+	public CreationNumericAdapter(Context context, List<Numeric> listNum) {
+		listNumeric = listNum;
+		mContext = context;
+		mInflater = LayoutInflater.from(mContext);
+	}
+
+	@Override
+	public int getCount() {
+		return listNumeric.size();
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		return listNumeric.get(arg0);
+	}
+
+	@Override
+	public long getItemId(int position) {
+
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+
+		return null;
+	}
+
+}
