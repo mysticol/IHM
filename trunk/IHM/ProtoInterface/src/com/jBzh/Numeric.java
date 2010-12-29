18cf844e9d02d615414373336403f4a09841ff92
anthony.caillaud <anthony.caillaud@e9c24a4e-4ec7-6425-f316-564117deb09f>
2010-12-29 12:26:02 +0000

    Ajout Numeric

---
+++ trunk/IHM/ProtoInterface/src/com/jBzh/Numeric.java
@@ -0,0 +1,32 @@
+package com.jBzh;
+
+public class Numeric {
+	
+	public String nomNumeric;
+	public int valeur;
+	
+	public Numeric(String nomNumeric, int valeur) {
+		super();
+		this.nomNumeric = nomNumeric;
+		this.valeur = valeur;
+	}
+	
+	public String getNomNumeric() {
+		return nomNumeric;
+	}
+	
+	public void setNomNumeric(String nomNumeric) {
+		this.nomNumeric = nomNumeric;
+	}
+	
+	public int getValeur() {
+		return valeur;
+	}
+	
+	public void setValeur(int valeur) {
+		this.valeur = valeur;
+	}
+	
+	
+
+}
