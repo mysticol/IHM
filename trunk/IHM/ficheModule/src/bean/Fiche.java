05b08aeddbbb3a0cac3c2ad2621643fab2e14bea
manoel.fortun@gmail.com <manoel.fortun@gmail.com@e9c24a4e-4ec7-6425-f316-564117deb09f>
2010-12-29 16:14:53 +0000

    putain de eclipse de merde

---
+++ trunk/IHM/ficheModule/src/bean/Fiche.java
@@ -0,0 +1,300 @@
+package bean;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Fiche {
+	
+	
+	private String nom;
+	private String poid;
+	private String taille;
+	private String age;
+	private String concept;
+	private String xp;
+	private String system;
+	private String univers;
+	private String campagne;
+	private HashMap<String, String> infos;
+	private HashMap<String, Caracteristique> caracteristiquesPrincipales;
+	private HashMap<String, Caracteristique> caracteristiquesSecondaire;
+	private HashMap<Categorie, LinkedList<Competence>> competences;
+	private LinkedList<Pouvoir> pouvoirs;
+	
+	private LinkedList<Equipement> equipements;
+	private Vie barreDeVie;
+	private String divers;
+	
+	
+	
+	
+
+
+
+	
+
+
+
+
+	public Fiche(String nom, String poid, String taille, String age,
+			String concept, String xp, String system, String univers,
+			String campagne, HashMap<String, String> infos,
+			HashMap<String, Caracteristique> caracteristiquesPrincipales,
+			HashMap<String, Caracteristique> caracteristiquesSecondaire,
+			HashMap<Categorie, LinkedList<Competence>> competences,
+			LinkedList<Pouvoir> pouvoirs, LinkedList<Equipement> equipements,
+			Vie barreDeVie, String divers) {
+		this.nom = nom;
+		this.poid = poid;
+		this.taille = taille;
+		this.age = age;
+		this.concept = concept;
+		this.xp = xp;
+		this.system = system;
+		this.univers = univers;
+		this.campagne = campagne;
+		this.infos = infos;
+		this.caracteristiquesPrincipales = caracteristiquesPrincipales;
+		this.caracteristiquesSecondaire = caracteristiquesSecondaire;
+		this.competences = competences;
+		this.pouvoirs = pouvoirs;
+		this.equipements = equipements;
+		this.barreDeVie = barreDeVie;
+		this.divers = divers;
+	}
+
+
+
+	public Fiche(String nom, String poid, String taille, String age,
+			String concept, String xp, String system, String univers,
+			String campagne, String divers) {
+		this.nom = nom;
+		this.poid = poid;
+		this.taille = taille;
+		this.age = age;
+		this.concept = concept;
+		this.xp = xp;
+		this.system = system;
+		this.univers = univers;
+		this.campagne = campagne;
+		this.divers = divers;
+	}
+
+
+
+	public String getSystem() {
+		return system;
+	}
+
+
+
+	public void setSystem(String system) {
+		this.system = system;
+	}
+
+
+
+	public String getUnivers() {
+		return univers;
+	}
+
+
+
+	public void setUnivers(String univers) {
+		this.univers = univers;
+	}
+
+
+
+	public String getCampagne() {
+		return campagne;
+	}
+
+
+
+	public void setCampagne(String campagne) {
+		this.campagne = campagne;
+	}
+
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+
+	public String getPoid() {
+		return poid;
+	}
+
+
+
+	public void setPoid(String poid) {
+		this.poid = poid;
+	}
+
+
+
+	public String getTaille() {
+		return taille;
+	}
+
+
+
+	public void setTaille(String taille) {
+		this.taille = taille;
+	}
+
+
+
+	public String getAge() {
+		return age;
+	}
+
+
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+
+
+	public String getConcept() {
+		return concept;
+	}
+
+
+
+	public void setConcept(String concept) {
+		this.concept = concept;
+	}
+
+
+
+	public String getXp() {
+		return xp;
+	}
+
+
+
+	public void setXp(String xp) {
+		this.xp = xp;
+	}
+
+
+
+	public HashMap<String, String> getInfos() {
+		return infos;
+	}
+
+
+
+	public void setInfos(HashMap<String, String> infos) {
+		this.infos = infos;
+	}
+
+
+
+	public HashMap<String, Caracteristique> getCaracteristiquesPrincipales() {
+		return caracteristiquesPrincipales;
+	}
+
+
+
+	public void setCaracteristiquesPrincipales(
+			HashMap<String, Caracteristique> caracteristiquesPrincipales) {
+		this.caracteristiquesPrincipales = caracteristiquesPrincipales;
+	}
+
+
+
+	public HashMap<String, Caracteristique> getCaracteristiquesSecondaire() {
+		return caracteristiquesSecondaire;
+	}
+
+
+
+	public void setCaracteristiquesSecondaire(
+			HashMap<String, Caracteristique> caracteristiquesSecondaire) {
+		this.caracteristiquesSecondaire = caracteristiquesSecondaire;
+	}
+
+
+
+	public LinkedList<Pouvoir> getPouvoirs() {
+		return pouvoirs;
+	}
+
+
+
+	public void setPouvoirs(LinkedList<Pouvoir> pouvoirs) {
+		this.pouvoirs = pouvoirs;
+	}
+
+
+
+	public LinkedList<Equipement> getEquipements() {
+		return equipements;
+	}
+
+
+
+	public void setEquipements(LinkedList<Equipement> equipements) {
+		this.equipements = equipements;
+	}
+
+
+
+	public Vie getBarreDeVie() {
+		return barreDeVie;
+	}
+
+
+
+	public void setBarreDeVie(Vie barreDeVie) {
+		this.barreDeVie = barreDeVie;
+	}
+
+
+
+	public String getDivers() {
+		return divers;
+	}
+
+
+
+	public void setDivers(String divers) {
+		this.divers = divers;
+	}
+
+
+
+	public Fiche() {
+		
+	}
+
+
+
+	public HashMap<Categorie, LinkedList<Competence>> getCompetences() {
+		return competences;
+	}
+
+
+
+	public void setCompetences(
+			HashMap<Categorie, LinkedList<Competence>> competences) {
+		this.competences = competences;
+	}
+	
+	
+	
+	
+}
---
+++ trunk/IHM/ficheModule/src/bean/Modele.java
@@ -0,0 +1,78 @@
+package bean;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Modele {
+
+	private String univers;
+	private String system;
+	private Integer vie;
+	private LinkedList<Info> infos;
+	private LinkedList<Caracteristique> caractPrincipales;
+	private LinkedList<Caracteristique> caractSecondaires;
+	private HashMap<Categorie, LinkedList<Competence>> competences;
+	public LinkedList<Info> getInfos() {
+		return infos;
+	}
+	public void setInfos(LinkedList<Info> infos) {
+		this.infos = infos;
+	}
+	public LinkedList<Caracteristique> getCaractPrincipales() {
+		return caractPrincipales;
+	}
+	public void setCaractPrincipales(LinkedList<Caracteristique> caractPrincipales) {
+		this.caractPrincipales = caractPrincipales;
+	}
+	public LinkedList<Caracteristique> getCaractSecondaires() {
+		return caractSecondaires;
+	}
+	public void setCaractSecondaires(LinkedList<Caracteristique> caractSecondaires) {
+		this.caractSecondaires = caractSecondaires;
+	}
+	public HashMap<Categorie, LinkedList<Competence>> getCompetences() {
+		return competences;
+	}
+	public void setCompetences(
+			HashMap<Categorie, LinkedList<Competence>> competences) {
+		this.competences = competences;
+	}
+
+	public String getUnivers() {
+		return univers;
+	}
+	public void setUnivers(String univers) {
+		this.univers = univers;
+	}
+	public String getSystem() {
+		return system;
+	}
+	public void setSystem(String system) {
+		this.system = system;
+	}
+	public Modele() {
+	
+	}
+	public Modele(String univers, String system,Integer vie, LinkedList<Info> infos,
+			LinkedList<Caracteristique> caractPrincipales,
+			LinkedList<Caracteristique> caractSecondaires,
+			HashMap<Categorie, LinkedList<Competence>> competences) {
+		this.vie=vie;
+		this.univers = univers;
+		this.system = system;
+		this.infos = infos;
+		this.caractPrincipales = caractPrincipales;
+		this.caractSecondaires = caractSecondaires;
+		this.competences = competences;
+	}
+	public Integer getVie() {
+		return vie;
+	}
+	public void setVie(Integer vie) {
+		this.vie = vie;
+	}
+	
+	
+	
+	
+}
