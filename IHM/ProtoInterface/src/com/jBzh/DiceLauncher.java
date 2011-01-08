6ac919d6b4919f1d868e20c6a096b1d47a0d8777
dejean.charles <dejean.charles@e9c24a4e-4ec7-6425-f316-564117deb09f>
2011-01-08 10:22:05 +0000

    selectionJet fini

--- IHM/ProtoInterface/src/com/jBzh/DiceLauncher.java
+++ IHM/ProtoInterface/src/com/jBzh/DiceLauncher.java
@@ -1,29 +1,49 @@
 package com.jBzh;
 
-import android.app.Activity;
+import dice.Dice;
+import dice.DiceType;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
-import dice.Dice;
-import dice.DiceType;
 
 public class DiceLauncher extends Activity {
 	/** Called when the activity is first created. */
-   @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	  super.onCreate(savedInstanceState);
         
-        //setContentView(R.layout.main);
-    
-        System.out.println("<<<<<<<<<<<<<<<<<<<< 1");
+        setContentView(R.layout.dicelauncher);
         
-        final Button buttonPlus = (Button) findViewById(R.id.nbDicePlus);
-        buttonPlus.setOnClickListener(new View.OnClickListener() {
+        final Bundle objectbunble  = this.getIntent().getExtras();
+        
+        if(objectbunble.containsKey("diceType")){
+        	// si le jet est deja parametrer on set les parametre directement
+        	
+        	// on recupere le nombre de dés a lancer
+        	TextView nbDice = (TextView) findViewById(R.id.nbDiceValue);
+        	Integer newVal = objectbunble.getInt("nbDice");
+        	nbDice.setText(String.valueOf(newVal));
+   
+        	// on recupere le type de dés
+        	String diceTypeString = objectbunble.getString("diceType");
+        	Spinner diceTypeSpinner = (Spinner) findViewById(R.id.diceTypeSpinner);
+        	for(int i = 0 ; i < 10 ; i++){
+        		diceTypeSpinner.setSelection(i);
+        		if(diceTypeString.equalsIgnoreCase((String)diceTypeSpinner.getSelectedItem())){
+        			break;
+        		}
+        	}
+        }
+                
+        final Button buttonDicePlus = (Button) findViewById(R.id.nbDicePlus);
+        buttonDicePlus.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+            	
             	TextView nbDice = (TextView) findViewById(R.id.nbDiceValue);
             	
             	Integer newVal = Integer.valueOf(nbDice.getText().toString()) + 1;
@@ -32,10 +52,8 @@ public class DiceLauncher extends Activity {
             }
         });
         
-        System.out.println("<<<<<<<<<<<<<<<<<<<< 2");
-        
-        final Button buttonMoin = (Button) findViewById(R.id.nbDiceMoins);
-        buttonMoin.setOnClickListener(new View.OnClickListener() {
+        final Button buttonDiceMoin = (Button) findViewById(R.id.nbDiceMoins);
+        buttonDiceMoin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
             	TextView nbDice = (TextView) findViewById(R.id.nbDiceValue);
             	
@@ -47,8 +65,6 @@ public class DiceLauncher extends Activity {
             	
             }
         });
-        
-        System.out.println("<<<<<<<<<<<<<<<<<<<< 3");
         
         final Builder builder = new AlertDialog.Builder(this);
 	    final Button buttonLancer = (Button) findViewById(R.id.launchButton);
@@ -67,6 +83,5 @@ public class DiceLauncher extends Activity {
 	        	
 	        }
 	    }); 
-	    System.out.println("<<<<<<<<<<<<<<<<<<<< 4");
     }
 }
--- IHM/ProtoInterface/src/com/jBzh/test.java
+++ IHM/ProtoInterface/src/com/jBzh/test.java
@@ -20,12 +20,12 @@ public class test {
 		r.setName("wod1");
 		
 		LinkedList<String> le1 = new LinkedList<String>();
-		le1.add("caracPrim");
+		le1.add("caracP");
 		le1.add("comp");
 		r.addRoll("classique", new Roll(DiceType.D10,le1));
 		
 		LinkedList<String> le2 = new LinkedList<String>();
-		le2.add("caracSec");
+		le2.add("caracS");
 		r.addRoll("secondaire", new Roll(DiceType.D10,le2));
 		
 		r.addResult("default", new Result());
